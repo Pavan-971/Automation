db18aafc21fec41114fa557c43dc2e5a1fa1321c
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2020-08-07 16:37:32 +0530

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,4 +1,4 @@
-
+read -p "Enter your jenkins server ip: "  ip
 yum install java-1.8.0-openjdk.x86_64 -y
 sudo cp /etc/profile /etc/profile_backup
 echo 'export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk' | sudo tee -a /etc/profile
@@ -19,4 +19,16 @@ mkdir /var/lib/jenkins/init.groovy.d
 cp Automate.sh /var/lib/jenkins/init.groovy.d/basic-security.groovy
 service jenkins restart
 sleep 60
+
+#------------installing suggested plugins------------------------
+
+wget http://$ip:8080/jnlpJars/jenkins-cli.jar
+content=$(tail -n +2 plugin.txt)
+for line in $content; do
+         var=$(echo $line|tr -d '\n')
+
+         java -jar jenkins-cli.jar -s http://$ip:8080 -auth pavan:pavan@1 install-plugin  $var
+done < plugin.txt;
+service jenkins restart
+© 2020 GitHub, Inc.
 

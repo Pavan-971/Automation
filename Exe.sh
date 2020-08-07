8540473bdb6cc2b99564dc475e82be877917ef47
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2020-08-07 16:26:40 +0530

    Update Exe.sh

--- Exe.sh
+++ Exe.sh
@@ -1,6 +1,9 @@
+#------------installing suggested plugins------------------------
+wget http://$ip:8080/jnlpJars/jenkins-cli.jar
 content=$(tail -n +2 plugin.txt)
 for line in $content; do
          var=$(echo $line|tr -d '\n')
 
-         java -jar jenkins-cli.jar -s http://18.219.212.63:8080 -auth pavan:pavan@1 install-plugin  $var
+         java -jar jenkins-cli.jar -s http://$ip:8080 -auth pavan:pavan@1 install-plugin  $var
 done < plugin.txt;
+service jenkins restart

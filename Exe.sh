bd0d690eb7460901dd8e7c2e3a04d04ecdad479b
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2020-08-07 13:40:56 +0530

    Update Exe.sh

--- Exe.sh
+++ Exe.sh
@@ -1,8 +1,8 @@
-val=$(wc -l < plugins.txt)
+val=$(wc -l < plugin.txt)
 
 while read line;
 do
         var=$(echo $line|tr -d '\n')
         echo $line
         java -jar jenkins-cli.jar -s http://18.219.212.63:8080 -auth pavan:pavan@1 install-plugin  $var
-done < plugins.txt;
+done < plugin.txt;

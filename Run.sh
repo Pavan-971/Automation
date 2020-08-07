782b122fb23a976b29879f03b124b076f8202f2e
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2020-08-07 16:26:13 +0530

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -17,12 +17,5 @@ service jenkins restart
 #------------unlock jenkins and create admin user in jenkins-------
 mkdir /var/lib/jenkins/init.groovy.d
 cp Automate.sh /var/lib/jenkins/init.groovy.d/basic-security.groovy
-#------------installing suggested plugins------------------------
-wget http://$ip:8080/jnlpJars/jenkins-cli.jar
-content=$(tail -n +2 plugin.txt)
-for line in $content; do
-         var=$(echo $line|tr -d '\n')
-
-         java -jar jenkins-cli.jar -s http://$ip:8080 -auth pavan:pavan@1 install-plugin  $var
-done < plugin.txt;
 service jenkins restart
+
